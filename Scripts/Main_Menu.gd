6b7e8b4d1b6bb5ba3a9b8bfca6db2632f5e5b2ad
hojun313 <hojun313@naver.com>
2025-07-21 13:34:22 +0900

    준비

--- Scripts/Main_Menu.gd
+++ Scripts/Main_Menu.gd
@@ -4,4 +4,4 @@ func _ready():
 	$StartButton.pressed.connect(on_start_button_pressed)
 
 func on_start_button_pressed():
-	Game_Manager.goto_scene("res://Boss_Battle_1.tscn")
+	Game_Manager.goto_scene("res://Scenes/Boss_Battle_1.tscn")
